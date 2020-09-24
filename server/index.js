28800852fab5653b1e00e21fcd7c486d9c0432fe
Catherine Straus <catherinestraus@berkeley.edu>
2020-09-24 14:52:38 -0700

    Edited Seeding

--- server/index.js
+++ server/index.js
@@ -1,3 +1,20 @@
 const express = require('express');
 const app = express();
-const db = require('../database/index')
+const Review = require('../database/index')
+
+const port = 3003
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`)
+})
+
+app.get('/api/reviews', function (req, res) {
+  Review.find()
+    .sort( { dateNum : -1 } )
+    .then((data) => {
+      res.json(data)
+    })
+  }
+)
+
+
+
