aac90bb44a50d670ff70f64e9d70dada0ea7e208
Catherine Straus <catherinestraus@berkeley.edu>
2020-09-24 15:43:38 -0700

    Completed Envrionment Setup

--- server/index.js
+++ server/index.js
@@ -2,10 +2,13 @@ const express = require('express');
 const app = express();
 const Review = require('../database/index')
 
+app.use(express.static(__dirname + '/../client/dist'));
+
 const port = 3003
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`)
 })
+
 
 app.get('/api/reviews', function (req, res) {
   Review.find()
