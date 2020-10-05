d47308a86e37b6f623ee0b14dc1324b6efe37ffc
Catherine Straus <catherinestraus@berkeley.edu>
2020-10-05 16:28:21 -0700

    Added webpack configuration to copy over index.html and images directory

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 var path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
 var SRC_DIR = path.join(__dirname, '/client/src');
 var DIST_DIR = path.join(__dirname, '/client/dist');
 
@@ -6,7 +8,7 @@ module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
   output: {
     filename: 'bundle.js',
-    path: DIST_DIR
+    path: DIST_DIR,
   },
   module: {
     rules: [
@@ -14,9 +16,23 @@ module.exports = {
         test: /\.jsx?/,
         loader: 'babel-loader',
         options: {
-          'presets': ['@babel/preset-env', '@babel/preset-react']
-        }
-      }
-    ]
-  }
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: `${SRC_DIR}/index.html`,
+          to: `${DIST_DIR}/index.html`,
+        },
+        {
+          from: `${SRC_DIR}/images/pink-star.png`,
+          to: `${DIST_DIR}/images/pink-star.png`,
+        },
+      ],
+    }),
+  ],
 };
