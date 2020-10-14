e59b98a5978687c71f7a9ec82190a549369bea81
Elana Cohen-Khani <elanadev@gmail.com>
2020-10-14 12:47:43 -0700

    Installed AirBnB linter

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const bodyparser = require('body-parser')
 const Review = require('../database/index');
 
 app.use(express.static(__dirname + '/../client/dist'));
