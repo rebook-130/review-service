112507bff446645888b280aa3cb70c6f550e8551
Elana Cohen-Khani <elanadev@gmail.com>
2020-10-14 11:51:36 -0700

    Change database/index.js to point to local mongo database

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://db:27017/bookable');
+mongoose.connect('mongodb://localhost:27017/bookable');
 
 const reviewSchema = mongoose.Schema({
   username: String,
