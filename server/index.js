a82baf0437bc10e457cbf6c3bbb3d6948b37df1e
Elana Cohen-Khani <elanadev@gmail.com>
2020-10-14 14:09:50 -0700

    Put back changes

--- server/index.js
+++ server/index.js
@@ -1,24 +1,27 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+<<<<<<< HEAD
 const bodyparser = require('body-parser')
+=======
+const bodyparser = require('body-parser');
+>>>>>>> 8d115fd... Put back changes
 const Review = require('../database/index');
-const bodyparser = require('body-parser')
 
-app.use(express.static(__dirname + '/../client/dist'));
-app.use(bodyparser.json())
+app.use(express.static(`${__dirname}/../client/dist`));
+app.use(bodyparser.json());
 
 const port = 3003;
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
 });
 
-app.get('/rooms/*', function (req, res) {
+app.get('/rooms/*', (req, res) => {
   res.setHeader('content-type', 'text/html');
-  res.sendFile(path.resolve(__dirname + '/../client/dist/index.html'));
+  res.sendFile(path.resolve(`${__dirname}/../client/dist/index.html`));
 });
 
-app.get('/api/rooms/:roomId', function (req, res) {
+app.get('/api/rooms/:roomId', (req, res) => {
   Review.find({
     roomId: req.params.roomId,
   })
@@ -28,35 +31,33 @@ app.get('/api/rooms/:roomId', function (req, res) {
     });
 });
 
-app.post('/api/rooms/:roomId', function (req, res) {
-  const newReview = req.body
-  console.log(newReview)
+app.post('/api/rooms/:roomId', (req, res) => {
+  const newReview = req.body;
+  console.log(newReview);
   Review.create({
     roomId: req.params.roomId,
-    ...newReview
+    ...newReview,
   })
-    .then(data => { res.json(data) })
-    .catch(err => console.error(err))
+    .then((data) => { res.json(data); })
+    .catch((err) => console.error(err));
 });
 
-app.post('/api/rooms/:roomId', function (req, res) {
-  const newReview = req.body
-  console.log(newReview)
+app.post('/api/rooms/:roomId', (req, res) => {
+  const newReview = req.body;
   Review.create({
     roomId: req.params.roomId,
-    ...newReview
+    ...newReview,
   })
-    .then(data => { res.json(data) })
-    .catch(err => console.error(err))
+    .then((data) => { res.json(data); })
+    .catch((err) => console.error(err));
 });
 
-app.patch('/api/rooms/:roomId', function (req, res) {
-  const updatedReview = req.body
-  console.log(updatedReview)
+app.patch('/api/rooms/:roomId', (req, res) => {
+  const updatedReview = req.body;
   Review.create({
     roomId: req.params.roomId,
-    ...updatedReview
+    ...updatedReview,
   })
-    .then(data => { res.json(data) })
-    .catch(err => console.error(err))
+    .then((data) => { res.json(data); })
+    .catch((err) => console.error(err));
 });
