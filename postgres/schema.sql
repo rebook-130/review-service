4f7e31fbf39b6cd6cfaabd4fd10b2ad8e944bf2b
Elana Cohen-Khani <elanadev@gmail.com>
2020-10-21 23:53:35 -0700

    Remove host table and references to host_id from postgres schema
    Current setup can succesfully load data into postgres

--- postgres/schema.sql
+++ postgres/schema.sql
@@ -1,30 +1,31 @@
-DROP DATABASE IF EXISTS reviews;
+-- DROP DATABASE IF EXISTS reviews;
 
-CREATE DATABASE reviews;
+-- CREATE DATABASE reviews;
 
 \c reviews;
 
-CREATE TABLE users(
-  user_id SERIAL PRIMARY KEY,
-  first_name VARCHAR(100),
-  last_name VARCHAR(100),
-  rating NUMERIC(4,1),
-  avatar_url TEXT);
+-- CREATE TABLE users(
+--   user_id SERIAL PRIMARY KEY,
+--   first_name VARCHAR(100),
+--   last_name VARCHAR(100),
+--   rating NUMERIC(4,1),
+--   avatar_url TEXT);
 
-CREATE TABLE listings(
-  listing_id SERIAL PRIMARY KEY,
-  title VARCHAR(100) NOT NULL,
-  descrip TEXT NOT NULL,
-  bedrooms SMALLINT NOT NULL,
-  bathrooms SMALLINT NOT NULL,
-  beds SMALLINT NOT NULL,
-  maxguests SMALLINT NOT NULL,
-  variableprice BOOLEAN NOT NULL,
-  plus BOOLEAN NOT NULL,
-  cancellation_policy VARCHAR(100) NOT NULL,
-  lowest_price NUMERIC(7,2)
-  );
+-- CREATE TABLE listings(
+--   listing_id SERIAL PRIMARY KEY,
+--   title VARCHAR(100) NOT NULL,
+--   descrip TEXT NOT NULL,
+--   bedrooms SMALLINT NOT NULL,
+--   bathrooms SMALLINT NOT NULL,
+--   beds SMALLINT NOT NULL,
+--   maxguests SMALLINT NOT NULL,
+--   variableprice BOOLEAN NOT NULL,
+--   plus BOOLEAN NOT NULL,
+--   cancellation_policy VARCHAR(100) NOT NULL,
+--   lowest_price NUMERIC(7,2)
+--   );
 
+DROP TABLE reviews;
 CREATE TABLE reviews(
   review_id SERIAL PRIMARY KEY,
   review_text TEXT NOT NULL,
@@ -38,16 +39,16 @@ CREATE TABLE reviews(
   location SMALLINT NOT NULL,
   value SMALLINT NOT NULL,
 
-  listing_id INTEGER NOT NULL REFERENCES listings ON DELETE CASCADE,
-  user_id INTEGER NOT NULL REFERENCES users ON DELETE CASCADE);
+  listing_id INTEGER NOT NULL /*REFERENCES listings ON DELETE CASCADE*/,
+  user_id INTEGER NOT NULL /*REFERENCES users ON DELETE CASCADE*/);
 
-COPY users(first_name,last_name,rating,avatar_url)
-FROM '/Volumes/sdc/postgres/users.csv'
-WITH (format csv);
+-- COPY users(first_name,last_name,rating,avatar_url)
+-- FROM '/Volumes/sdc/postgres/users.csv'
+-- WITH (format csv);
 
-COPY listings(title,descrip,bedrooms,bathrooms,beds,maxguests,variableprice,plus,cancellation_policy,lowest_price,host_id)
-FROM '/Volumes/sdc/postgres/listings.csv'
-WITH (format csv);
+-- COPY listings(title,descrip,bedrooms,bathrooms,beds,maxguests,variableprice,plus,cancellation_policy,lowest_price)
+-- FROM '/Volumes/sdc/postgres/listings.csv'
+-- WITH (format csv);
 
 COPY reviews(review_text,time_stamp,time_formatted,cleanliness,communication,checkinrating,accuracy,location,value,listing_id,user_id)
 FROM '/Volumes/sdc/postgres/reviews.csv'
