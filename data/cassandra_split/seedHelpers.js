31dd248d0ff23b3198faa22decb2881798b0026b
Elana Cohen-Khani <elanadev@gmail.com>
2020-10-24 00:07:26 -0700

    Commit before cleanup

---
+++ data/cassandra_split/seedHelpers.js
@@ -0,0 +1,71 @@
+const { LoremIpsum } = require('lorem-ipsum');
+const dayjs = require('dayjs');
+
+function createTitle() {
+  const adjective = ['Charming', 'Cozy', 'Elegant', 'Chic', 'Eco', 'Stunning', 'Gorgeous', 'Boutique', 'Adorable', 'Dog-Friendly', 'Beautiful', 'Private', 'Fabulous', 'Spacious', 'Comfortable', 'Relaxing', 'Rustic', 'Zen'];
+
+  const noun = ['House', 'Cottage', 'Castle', 'Estate', 'Treehouse', 'Yurt', 'Tent', 'Bungalow', 'Cabin', 'Room', 'Architectural Wonder', 'Suite', 'Retreat', 'Suite', 'Apartment', 'Refuge', 'Villa'];
+
+  const location = ['in the Woods', 'in the Redwoods', 'in the Mountains', 'near the Mountains', 'on the Beach', 'by the Ocean', 'in the City Center', 'Near All the Landmarks', 'By the Sea', 'on the Moors', 'in the Desert', 'in the Jungle'];
+
+  // for (let i = 0; i < 100; i += 1) {
+  const randomAdjective = Math.floor(Math.random() * adjective.length);
+  const randomNoun = Math.floor(Math.random() * noun.length);
+  const randomLocation = Math.floor(Math.random() * location.length);
+
+  return `${adjective[randomAdjective]} ${noun[randomNoun]} ${location[randomLocation]}`;
+  // }
+}
+
+function cancellation() {
+  const random = Math.floor(Math.random() * 6 + 1);
+
+  let policy = '';
+
+  if (random === 1) {
+    policy = 'flexible';
+  } else if (random === 2) {
+    policy = 'moderate';
+  } else if (random === 3) {
+    policy = 'strict';
+  } else if (random === 4) {
+    policy = 'longterm';
+  } else if (random === 5) {
+    policy = 'super30';
+  } else if (random === 6) {
+    policy = 'super60';
+  }
+
+  return policy;
+}
+
+function paragraphs() {
+  const lorem = new LoremIpsum({
+    sentencesPerParagraph: {
+      max: 4,
+      min: 2,
+    },
+    wordsPerSentence: {
+      max: 12,
+      min: 4,
+    },
+  });
+
+  return lorem.generateParagraphs(Math.ceil((Math.random() * 3 - 1) + 1));
+}
+
+function dates() {
+  const earliest = 1451606400;
+  const current = Math.ceil(Date.now() / 1000);
+  const randomEpoch = Math.ceil(((Math.random() * (current - earliest)) + earliest));
+
+  let date = new Date(0);
+  date = new Date(date.setUTCSeconds(randomEpoch)).toUTCString();
+  const dateFormat = dayjs(date).format('MMMM YYYY');
+
+  return { epoch: randomEpoch, dateFormat };
+}
+
+module.exports = {
+  createTitle, cancellation, paragraphs, dates,
+};
---
+++ data/cassandra_split/seedReviews.js
@@ -0,0 +1,56 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable camelcase */
+/* eslint-disable no-console */
+const faker = require('faker');
+const generator = require('../generatorCsv');
+
+const {
+  createTitle, cancellation, paragraphs, dates,
+} = require('../seedHelpers');
+
+const random = function random(min, max) { return Math.ceil(Math.random() * (max - min) + min); };
+
+const createReviewText = () => {
+  const date = dates();
+  const line = {
+    id: 0,
+    listing_id: random(1, 5000000),
+    user_first_name: faker.name.firstName(),
+    user_avatar: faker.internet.avatar(),
+    review_text: paragraphs(),
+    time_stamp: date.epoch,
+    time_formatted: date.dateFormat,
+
+    cleanliness: random(2, 5),
+    communication: random(2, 5),
+    checkinrating: random(2, 5),
+    accuracy: random(2, 5),
+    location: random(2, 5),
+    value: random(2, 5),
+    // zip_code: faker.address.zipCode(),
+    // bedrooms: random(1, 6),
+  };
+
+  const header = Object.keys(line);
+  return { line, header };
+};
+
+const createReviewScores = () => {
+  const line = {
+    id: 0,
+    cleanliness_avg: random(2, 5),
+    communication_avg: random(2, 5),
+    checkinrating_avg: random(2, 5),
+    accuracy_avg: random(2, 5),
+    location_avg: random(2, 5),
+    value_avg: random(2, 5),
+    overall_avg: random(2, 5),
+  };
+
+  const header = Object.keys(line);
+  return { line, header };
+};
+
+// Create 52 million reviews
+// generator('/Volumes/sdc/cassandra/review_text.csv', 50000000, createReviewText, 'reviews text');
+generator('/Volumes/sdc/cassandra_split/review_scores.csv', 5000000, createReviewScores, 'review scores');
