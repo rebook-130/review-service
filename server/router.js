af10fa60f69b12a3594363ad5d8656e43fc0084a
Elana Cohen-Khani <elanadev@gmail.com>
2020-10-24 23:52:46 -0700

    Refactor API routes to use Cassandra

---
+++ server/router.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const Routes = require('./controller.js');
+
+const router = express.Router();
+
+router.get('/listing*', (req, res) => {
+  res.setHeader('content-type', 'text/html');
+  res.sendFile(path.resolve(`${__dirname}/../client/dist/index.html`));
+});
+
+router.get('/api/listing/:listingId/reviews', Routes.getReviews);
+
+router.get('/api/listing/:listingId/scores', Routes.getScores);
+
+module.exports = router;
