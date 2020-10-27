000236eaf0cb3760e9c774662caaea456badc2f0
Elana Cohen-Khani <elanadev@gmail.com>
2020-10-26 22:03:25 -0700

    Mods made for testing

--- server/server.js
+++ server/server.js
@@ -6,7 +6,13 @@ const router = require('./router');
 
 const app = express();
 
-app.use(express.static(`${__dirname}/../client/dist`));
+//app.use(express.static(`${__dirname}/../client/dist`));
+
+// router.get('/listing*', (req, res) => {
+//   // res.setHeader('content-type', 'text/html');
+//   res.sendFile(path.resolve(`${__dirname}/../client/dist/index.html`));
+// });
+
 app.use(bodyparser.json());
 
 const port = 3003;
@@ -14,7 +20,7 @@ app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
 });
 
-app.use('/', router);
+app.use('/api', router);
 
 // app.post('/api/rooms/:roomId/reviews', (req, res) => {
 //   const newReview = req.body;
